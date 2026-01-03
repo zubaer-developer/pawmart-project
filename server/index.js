264b2827273ee641d7f1cdea666590762a8da92d
ZUBAER <zubaer.developer@gmail.com>
2026-01-03 12:50:06 +0600

    database connection successfully setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // app initialize
 const app = express();
@@ -11,10 +12,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Testing route
-app.get("/", (req, res) => {
-  res.send("PawMart Server is Running");
+// --- MongoDB connection logic ---
+const uri = process.env.MONGODB_URI;
+// Create a MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // ------------------
+    //  (API Endpoints)
+    // ------------------
+
+    // Testing route
+    app.get("/", (req, res) => {
+      res.send("PawMart Server is Running and connected to MongoDB");
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 // For local testing
 app.listen(port, () => {
