74fc3579b989b185ed9b5b4f05a7a698b701d4f9
ZUBAER <zubaer.developer@gmail.com>
2026-01-03 09:33:41 +0600

    client side and server side project setup with necessary files

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// require necessary Files
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+// app initialize
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware configuration
+app.use(cors());
+app.use(express.json());
+
+// Testing route
+app.get("/", (req, res) => {
+  res.send("PawMart Server is Running");
+});
+
+// listen port
+app.listen(port, () => {
+  console.log(`PawMart Server is running on Port: ${port}`);
+});
