a9c707267245724f4739d95d5ecd9a6556219b96
ZUBAER <zubaer.developer@gmail.com>
2026-01-03 12:07:15 +0600

    vercel api debug test

--- api/index.js
+++ api/index.js
@@ -1,29 +1,9 @@
-// require necessary Files
 const express = require("express");
-const cors = require("cors");
-const apiRouter = require("./routes/api");
-require("dotenv").config();
 
-// app initialize
 const app = express();
-const port = process.env.PORT || 5000;
 
-// middleware configuration
-app.use(cors());
-app.use(express.json());
-app.use("/api", apiRouter);
-
-// Testing route
-app.get("/", (req, res) => {
-  res.send("PawMart Server is Running");
+app.get("/api", (req, res) => {
+  res.send("API WORKING");
 });
 
-// For local testing
-if (require.main === module) {
-  app.listen(port, () => {
-    console.log(`PawMart Server is running on Port: ${port}`);
-  });
-}
-
-// For Vercel deployment
 module.exports = app;
