5040887846306398358b990b4202c30adcb3225d
ZUBAER <zubaer.developer@gmail.com>
2026-01-03 16:14:22 +0600

    update main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
-import { router } from "./routes/routes";
+import { router } from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
